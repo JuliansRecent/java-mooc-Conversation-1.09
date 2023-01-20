adb6f359cdded9706c19dff46d88f693ab1e7d76
JuliansRecent <juliansrecent@gmail.com>
2023-01-20 06:05:20 -0800

    Module complete. Prompt the user for two inputs, store the responses in varibales, and return 3 responses back.

--- src/main/java/Conversation.java
+++ src/main/java/Conversation.java
@@ -7,6 +7,15 @@ public class Conversation {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        // Print a question to give the user a question to respond to
+        System.out.println("Greetings! How are you doing?");
+        // Prompt the user to give a response, store it in a String variable, and return it in the console
+        String first_response = scanner.nextLine();
+        // Print a response to their input to the first question, asking for another response
+        System.out.println("Oh, how interesting. Tell me more!");
+        // Prompt the user for a response again, store it in a String variable and return it in the console
+        String second_response = scanner.nextLine();
+        // Print a closing statement, letting the user know that the interaction is over
+        System.out.println("Thanks for sharing!");
     }
 }
